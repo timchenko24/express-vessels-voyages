4d26bfbc529e2359eb5596b7ea687fee00260c43
Tim Chenskiy <timadraft@gmail.com>
2020-11-16 19:15:22 +0300

    tests env setup

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 import express from 'express';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
 import indexRouter from './routes';
 
 const app = express();
 
+app.use(logger('dev'));
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+app.use(cookieParser());
 app.use('/', indexRouter);
 
 export default app;
-// app.get('/ports', getPorts)
-//
-// app.listen(port, () => {
-//     console.log(`App running on port ${port}.`)
-// })
