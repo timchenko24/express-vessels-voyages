a1175990313fc30be6e640f2a7b3c21734da60ef
Tim Chenskiy <timadraft@gmail.com>
2020-11-18 20:18:19 +0300

    ports page test

---
+++ test/ports.test.js
@@ -0,0 +1,20 @@
+import { expect, server, BASE_URL } from './setup';
+
+describe('Ports', () => {
+  it('get ports page', done => {
+    server
+      .get(`${BASE_URL}ports`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.ports).to.be.instanceOf(Array);
+        res.body.ports.forEach(m => {
+          expect(m).to.have.property('name');
+          expect(m).to.have.property('type');
+          expect(m).to.have.property('longitude');
+          expect(m).to.have.property('latitude');
+        });
+        done();
+      });
+  });
+});
