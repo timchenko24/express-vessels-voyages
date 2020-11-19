70c8f1ddc505e71f03a7ed3c8da13d9dd7b224dc
Tim Chenskiy <timadraft@gmail.com>
2020-11-19 18:07:59 +0300

    get port page by id and param query

--- src/controllers/ports.js
+++ src/controllers/ports.js
@@ -1,13 +1,30 @@
 import Model from '../models/model';
+import { generateClauseById, generateClauseByParams } from '../utils';
 
-const portsModel = new Model('dw_port');
+const portsModel = new Model('dw_port as p');
 
 export const portsPage = async (req, res) => {
   try {
+    let clause = '';
+
+    const params = {
+      country: (val) => `LOWER(c.name) = LOWER('${val}')`,
+      name: (val) => `LOWER(p.name) = LOWER('${val}')`,
+      longitude__lte: (val) => `p.longitude <= '${val}'`,
+      longitude__gte: (val) => `p.longitude >= '${val}'`,
+      latitude__lte: (val) => `p.longitude >= '${val}'`,
+      latitude__gte: (val) => `p.latitude >= '${val}'`
+    };
+
+    clause += generateClauseById(req, 'p.id');
+
+    clause += generateClauseByParams(req, params);
+
     const data = await portsModel.select(
       'p.id, c.name as country, p.name, p.type, p.longitude, p.latitude',
-      ' as p inner join dw_portcountry as c on p.country_id = c.id'
+      ` inner join dw_portcountry as c on p.country_id = c.id ${clause}`
     );
+
     res.status(200).json({ ports: data.rows });
   } catch (err) {
     res.status(200).json({ ports: err.stack });
